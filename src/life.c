3e9cfa9cc13b906210bd3ee83335c673296e1de6
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2024-02-15 12:43:30 -0800

    improved life

--- src/life.c
+++ src/life.c
@@ -13,6 +13,7 @@
 // Obligatory Example
 // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
+// RAM screen buffer
 uint8_t buf[320 / 8 * 180];
 
 static void setup()
@@ -22,21 +23,10 @@ static void setup()
     // Erase graphics
     RIA.addr0 = 0;
     RIA.step0 = 1;
-    for (unsigned i = sizeof(buf) / 8; i--;)
-    {
+    for (unsigned i = sizeof(buf); i--;)
         RIA.rw0 = 0;
-        RIA.rw0 = 0;
-        RIA.rw0 = 0;
-        RIA.rw0 = 0;
-        RIA.rw0 = 0;
-        RIA.rw0 = 0;
-        RIA.rw0 = 0;
-        RIA.rw0 = 0;
-    }
 
     // Configure graphics mode
-    // xram0_struct_set(0xFF00, vga_mode3_config_t, x_wrap, true);
-    // xram0_struct_set(0xFF00, vga_mode3_config_t, y_wrap, true);
     xram0_struct_set(0xFF00, vga_mode3_config_t, x_pos_px, 0);
     xram0_struct_set(0xFF00, vga_mode3_config_t, y_pos_px, 0);
     xram0_struct_set(0xFF00, vga_mode3_config_t, width_px, 320);
@@ -46,32 +36,15 @@ static void setup()
 
     // Program video system
     xregn(1, 0, 0, 1, 2);            // 320x240
-    xregn(1, 0, 1, 3, 3, 8, 0xFF00); // 1bpp lsb
+    xregn(1, 0, 1, 3, 3, 0, 0xFF00); // 1bpp
     xregn(1, 0, 1, 2, 0, 1);         // console overlay
-
-    // Program direct keyboard input
-    xregn(0, 0, 0, 1, 0xFF10);
-
-    // Hi
-    printf("Conway's Game of Life");
-}
-
-static void waitkey()
-{
-    RIA.addr0 = 0xFF10;
-    RIA.step0 = 0;
-    while (!(RIA.rw0 & 1))
-        ;
-    while (RIA.rw0 & 1)
-        ;
-    printf("\n");
 }
 
 static inline void set(int x, int y, bool state)
 {
     RIA.addr0 = (x / 8) + (320 / 8 * y);
     RIA.step0 = 0;
-    uint8_t bit = 1 << (x % 8);
+    uint8_t bit = 128 >> (x % 8);
     if (state)
         RIA.rw0 |= bit;
     else
@@ -80,22 +53,23 @@ static inline void set(int x, int y, bool state)
 
 static inline uint8_t get(int x, int y)
 {
-    if (x < 0 || x >= 320 || y < 0 || y >= 180)
-        return 0;
     unsigned addr = (x / 8) + (320 / 8 * y);
-    uint8_t bit = 1 << (x % 8);
+    uint8_t bit = 128 >> (x % 8);
     return buf[addr] & bit ? 1 : 0;
 }
 
 static void next()
 {
+    // Copy screen to memory buffer
     RIA.addr0 = 0;
     RIA.step0 = 1;
     for (unsigned i = 0; i < sizeof(buf); i++)
         buf[i] = RIA.rw0;
-    for (int x = 0; x < 320; x++)
+
+    // Process all pixels excluding the edges
+    for (int x = 1; x < 319; x++)
     {
-        for (int y = 0; y < 180; y++)
+        for (int y = 1; y < 179; y++)
         {
             uint8_t neighbors =
                 get(x - 1, y - 1) +
@@ -121,9 +95,9 @@ static void next()
     }
 }
 
-static void gen0()
+// Glider moves right and down
+static void glider()
 {
-    // glider
     set(11, 20, true);
     set(12, 21, true);
     set(10, 22, true);
@@ -131,15 +105,41 @@ static void gen0()
     set(12, 22, true);
 }
 
+// Light-weight spaceship moves right
+static void lwss()
+{
+    set(20, 20, true);
+    set(23, 20, true);
+    set(24, 21, true);
+    set(20, 22, true);
+    set(24, 22, true);
+    set(21, 23, true);
+    set(22, 23, true);
+    set(23, 23, true);
+    set(24, 23, true);
+}
+
 int main()
 {
     setup();
 
-    gen0();
+    // Hi
+    printf("Conway's Game of Life");
 
-    for (int i = 0; i < 100; i++)
+    // Starting generation
+    glider();
+    lwss();
+
+    // Iterate generations
+    for (int i = 0; i < 500; i++)
         next();
 
+    // Done
     printf("\nPress any key to exit");
-    waitkey();
+    xregn(0, 0, 0, 1, 0xFF10);
+    RIA.addr0 = 0xFF10;
+    RIA.step0 = 0;
+    while (RIA.rw0 & 1)
+        ;
+    printf("\n");
 }
